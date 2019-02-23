7b1a9ca97424d8699602d7372ae9733ff4700471
Jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-02-23 16:31:32 +0100

    Update EnrollServiceTest

--- src/test/java/services/EnrollServiceTest.java
+++ src/test/java/services/EnrollServiceTest.java
@@ -13,15 +13,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
-import repositories.BrotherhoodRepository;
-
-import domain.Actor;
+import utilities.AbstractTest;
 import domain.Brotherhood;
 import domain.Enroll;
 import domain.Member;
-import domain.SocialProfile;
-
-import utilities.AbstractTest;
+import domain.Position;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -32,27 +28,30 @@ public class EnrollServiceTest extends AbstractTest {
 
 	//Service-------------------------------------------
 	@Autowired
-	private EnrollService	enrollService;
-	
+	private EnrollService		enrollService;
+
 	@Autowired
-	private MemberService	memberService;
-	
+	private MemberService		memberService;
+
 	@Autowired
 	private BrotherhoodService	brotherhoodService;
+
+	@Autowired
+	private PositionService		positionService;
 
 
 	//Test
 	@Test
-	public void testCreate(){
+	public void testCreate() {
 		System.out.println("========== testCreate() ==========");
 		final int memberId = this.getEntityId("member1");
 		final Member member = this.memberService.findOne(memberId);
 		final int brotherhoodId = this.getEntityId("brotherhood1");
 		final Brotherhood brotherhood = this.brotherhoodService.findOne(brotherhoodId);
+		final int positionId = this.getEntityId("position1");
+		final Position position = this.positionService.findOne(positionId);
 		try {
 			final Enroll e = this.enrollService.create(memberId, brotherhoodId);
-//			e.setEndMoment(new Date());
-//			e.setPosition("Position1");			
 			Assert.notNull(e);
 
 			System.out.println("¡Exito!");
@@ -62,87 +61,89 @@ public class EnrollServiceTest extends AbstractTest {
 		}
 
 	}
-		
+
+	@Test
+	public void testFindOne() {
+		System.out.println("========== testFindOne() ==========");
+		final int enrollId = this.getEntityId("enroll1");
+
+		try {
+			final Enroll enroll = this.enrollService.findOne(enrollId);
+			Assert.notNull(enroll);
+
+			System.out.println("¡Exito!");
+
+		} catch (final Exception e) {
+			System.out.println("¡Fallo," + e.getMessage() + "!");
+		}
+
+	}
+	@Test
+	public void testSave() {
+		System.out.println("========== testSave() ==========");
+		Enroll enroll, saved;
+		this.authenticate("brotherhood1");
+		final Collection<Enroll> enrolls;
+		final int brotherhoodId = this.getEntityId("brotherhood1");
+		final Brotherhood brotherhood = this.brotherhoodService.findOne(brotherhoodId);
+		final int memberId = this.getEntityId("member1");
+		final Member member = this.memberService.findOne(memberId);
+		final int positionId = this.getEntityId("position1");
+		final Position position = this.positionService.findOne(positionId);
+		try {
+			enroll = this.enrollService.create(memberId, brotherhoodId);
+			enroll.setHaSalido(false);
+			enroll.setPosition(position);
+			if (enroll.getHaSalido() == true)
+				enroll.setStartMoment(new Date(System.currentTimeMillis() - 1000));
+			if (enroll.getHaSalido() == true)
+				enroll.setEndMoment(new Date(System.currentTimeMillis() - 1000));
+			saved = this.enrollService.save(enroll);
+
+			enrolls = this.enrollService.findAll();
+			Assert.isTrue(enrolls.contains(saved));
+			System.out.println("¡Exito!");
+
+		} catch (final Exception e) {
+			System.out.println("¡Fallo," + e.getMessage() + "!");
+		}
+		this.unauthenticate();
+	}
+	@Test
+	public void testFindAll() {
+		System.out.println("========== testFindAll() ==========");
+		final int enrollId = this.getEntityId("Enroll1");
+		try {
+			final Enroll enroll = this.enrollService.findOne(enrollId);
+			final Collection<Enroll> enrolls = this.enrollService.findAll();
+			Assert.isTrue(enrolls.contains(enroll));
+			System.out.println("¡Exito!");
+
+		} catch (final Exception e) {
+			System.out.println("¡Fallo," + e.getMessage() + "!");
+		}
+
 	}
 
-@Test
-public void testFindOne() {
-	System.out.println("========== testFindOne() ==========");
-	final int profileId = this.getEntityId("profile1");
+	@Test
+	public void testDelete() {
+		System.out.println("========== testDelete() ==========");
+		this.authenticate("referee1");
+		final int enrollId = this.getEntityId("Enroll1");
 
-	try {
-		final SocialProfile profile = this.profileService.findOne(profileId);
-		Assert.notNull(profile);
+		try {
+			final Enroll enroll = this.enrollService.findOne(enrollId);
+			this.enrollService.delete(enroll);
+			final Collection<Enroll> enrolls = this.enrollService.findAll();
+			Assert.notNull(enrolls);
+			Assert.isTrue(!enrolls.contains(enroll));
 
-		System.out.println("¡Exito!");
+			System.out.println("¡Exito!");
 
-	} catch (final Exception e) {
-		System.out.println("¡Fallo," + e.getMessage() + "!");
+		} catch (final Exception e) {
+			System.out.println("¡Fallo," + e.getMessage() + "!");
+		}
+		this.unauthenticate();
 	}
-
-}
-@Test
-public void testSave() {
-	System.out.println("========== testSave() ==========");
-	SocialProfile profile, saved;
-	this.authenticate("member1");
-	final Collection<SocialProfile> profiles;
-	final int actorId = this.getEntityId("member1");
-	final Actor actor = this.actorService.findOne(actorId);
-	try {
-		profile = this.profileService.create(actorId);
-		profile.setNameSN("Antonia");
-		profile.setLinkSN("http://profile10.com");
-		profile.setNick("Antoñita97");
-		profile.setActor(actor);
-		profile.setNick("piwflow2");
-		saved = this.profileService.save(profile);
-
-		profiles = this.profileService.findAll();
-		Assert.isTrue(profiles.contains(saved));
-		System.out.println("¡Exito!");
-
-	} catch (final Exception e) {
-		System.out.println("¡Fallo," + e.getMessage() + "!");
-	}
-	this.unauthenticate();
-}
-
-@Test
-public void testFindAll() {
-	System.out.println("========== testFindAll() ==========");
-	final int profileId = this.getEntityId("socialProfile1");
-	try {
-		final SocialProfile profile = this.profileService.findOne(profileId);
-		final Collection<SocialProfile> profiles = this.profileService.findAll();
-		Assert.isTrue(profiles.contains(profile));
-		System.out.println("¡Exito!");
-
-	} catch (final Exception e) {
-		System.out.println("¡Fallo," + e.getMessage() + "!");
-	}
-
-}
-
-@Test
-public void testDelete() {
-	System.out.println("========== testDelete() ==========");
-	this.authenticate("referee1");
-	final int profileId = this.getEntityId("socialProfile1");
-
-	try {
-		final SocialProfile profile = this.profileService.findOne(profileId);
-		this.profileService.delete(profile);
-		final Collection<SocialProfile> profiles = this.profileService.findAll();
-		Assert.notNull(profiles);
-		Assert.isTrue(!profiles.contains(profile));
-
-		System.out.println("¡Exito!");
-
-	} catch (final Exception e) {
-		System.out.println("¡Fallo," + e.getMessage() + "!");
-	}
-	this.unauthenticate();
-}
 
 }
