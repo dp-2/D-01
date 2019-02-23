77f04186cf0342d309c3a6b42ed85058a3a23f83
Jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-02-23 17:22:35 +0100

    Recambiando Jesús

--- src/main/java/repositories/EnrollRepository.java
+++ src/main/java/repositories/EnrollRepository.java
@@ -1,7 +1,10 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Enroll;
@@ -9,4 +12,9 @@ import domain.Enroll;
 @Repository
 public interface EnrollRepository extends JpaRepository<Enroll, Integer> {
 
+	@Query("select e from Enroll e where e.member.id = ?1")
+	Collection<Enroll> findEnrollByMember(int memberId);
+
+	@Query("select e from Enroll e where e.brotherhood.id = ?1")
+	Collection<Enroll> findEnrollByBrotherhood(int brotherhoodId);
 }
