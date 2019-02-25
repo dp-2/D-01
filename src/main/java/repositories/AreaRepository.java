31a93cf37e5c1b041a533b8d74a4f365b41b481f
alfalatam <alfonsoalarcontamayo27@gmail.com>
2019-02-25 18:53:32 +0100

    Update

--- src/main/java/repositories/AreaRepository.java
+++ src/main/java/repositories/AreaRepository.java
@@ -11,6 +11,7 @@ import domain.Area;
 public interface AreaRepository extends JpaRepository<Area, Integer> {
 
 	//QUERY B1
+	//TODO Esta query no funciona bien.
 	@Query("select count(a.brotherhood),avg(a.brotherhood), min(a.brotherhood), max(a.brotherhood), stddev(a.brotherhood) from Area a")
 	Double[] queryC2AVG();
 
