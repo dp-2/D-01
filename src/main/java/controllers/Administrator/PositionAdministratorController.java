d34f052ca06a1e2ea5f4832d0e8089f155146661
Jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-02-26 18:33:49 +0100

    Empezando Position

---
+++ src/main/java/controllers/Administrator/PositionAdministratorController.java
@@ -0,0 +1,127 @@
+
+package controllers.Administrator;
+
+import java.util.Collection;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.PositionService;
+import controllers.AbstractController;
+import domain.Position;
+
+@Controller
+@RequestMapping("/position/administrator")
+public class PositionAdministratorController extends AbstractController {
+
+	@Autowired
+	private PositionService	positionService;
+
+
+	//List------------------------------------------------------
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView modelAndView;
+
+		final Collection<Position> positions = this.positionService.findAll();
+
+		modelAndView = new ModelAndView("position/list");
+		modelAndView.addObject("positions", positions);
+		modelAndView.addObject("requestURI", "/position/administrator/list.do");
+
+		return modelAndView;
+
+	}
+
+	//Create
+	@RequestMapping(value = "/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		final Position position;
+		position = this.positionService.create();
+		result = this.createEditModelAndView(position);
+
+		return result;
+	}
+
+	// Edit ---------------------------------------------------------------
+
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int positionId) {
+		final ModelAndView result;
+		Position position;
+		position = this.positionService.findOne(positionId);
+		Assert.notNull(position);
+		result = this.createEditModelAndView(position);
+		return result;
+	}
+
+	//Save
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Position position, final BindingResult binding) {
+
+		ModelAndView result;
+
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(position);
+		else
+			try {
+				this.positionService.save(position);
+				result = new ModelAndView("redirect:/position/administrator/list.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(position, "position.commit.error");
+
+			}
+		return result;
+	}
+
+	// Delete----------------------------------------------------------------------
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(@Valid final Position position, final BindingResult binding) {
+		ModelAndView result;
+
+		try {
+			this.positionService.delete(position);
+			result = new ModelAndView("redirect:/position/administrator/list.do");
+		} catch (final Throwable oops) {
+			result = this.createEditModelAndView(position, "position.commit.error");
+		}
+		return result;
+	}
+
+	//CreateModelAndView
+
+	protected ModelAndView createEditModelAndView(final Position position) {
+		ModelAndView result;
+
+		result = this.createEditModelAndView(position, null);
+
+		return result;
+
+	}
+
+	protected ModelAndView createEditModelAndView(final Position position, final String message) {
+		final ModelAndView result;
+
+		final Collection<Position> lista = this.positionService.findAll();
+
+		result = new ModelAndView("position/edit");
+		result.addObject("position", position);
+		result.addObject("message", message);
+		result.addObject("isRead", false);
+		result.addObject("position", lista);
+
+		result.addObject("requestURI", "position/administrator/edit.do");
+
+		return result;
+	}
+
+}
