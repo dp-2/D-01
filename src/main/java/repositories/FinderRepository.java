8c447066617c35c3801e8e2414e4eec5c358f39f
alfalatam <alfonsoalarcontamayo27@gmail.com>
2019-03-05 17:19:03 +0100

    Arreglo

--- src/main/java/repositories/FinderRepository.java
+++ src/main/java/repositories/FinderRepository.java
@@ -1,8 +1,6 @@
 
 package repositories;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,8 +13,8 @@ public interface FinderRepository extends JpaRepository<Finder, Integer> {
 	@Query("select f from Finder f where f.member.id = ?1")
 	Finder findFinderByMemberId(int memberId);
 
-	@Query("select min(i),max(i),avg(i) from Finder f join f.processions.size i")
-	Collection<Double> finderStats();
+	//	@Query("select min(i),max(i),avg(i) from Finder f join f.processions.size i")
+	//	Collection<Double> finderStats();
 
 	//Query: "The ratio of the empty versus non-empty finders" Vamos a considerar un finder vacío cuando no tiene lastUpdate
 
